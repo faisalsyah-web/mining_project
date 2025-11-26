2a698470de8f4dfa766d04d33d6ac8c99c866d1d
faisalsyah-web <faisalsyahruddin57@gmail.com>
2025-11-26 23:03:12 +0900

    first testing

--- SQL/alter_table.sql
+++ SQL/alter_table.sql
@@ -1,3 +1,5 @@
+--Load CSV to Table
+
 COPY mining_extraction
 FROM 'D:\Portfolio\data analys\Project\mining_project\CSV\mining_extraction.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
@@ -34,12 +36,11 @@ COPY master_pits
 FROM 'D:\Portfolio\data analys\Project\mining_project\CSV\master_pits.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-
+--Define Foreign Key
 ALTER TABLE mining_extraction
 ADD CONSTRAINT fk_mining_extraction_equipment_id
 FOREIGN KEY (equipment_id)
 REFERENCES master_equipment (equipment_id);
-
 
 ALTER TABLE hauling_trips
 ADD CONSTRAINT fk_mining_extraction_equipment_id
@@ -66,4 +67,28 @@ ADD CONSTRAINT fk_mining_extraction_buyers_id
 FOREIGN KEY (buyer_id)
 REFERENCES master_buyers (buyer_id);
 
+ALTER TABLE stockpile_movements
+ADD CONSTRAINT fk_stockpile_movements_pit_id
+FOREIGN KEY (source)
+REFERENCES master_pits (pit_id);
+
+
+--Index Mining extraxtion
 CREATE INDEX idx_pit_id ON mining_extraction (pit_id);
+CREATE INDEX idx_equipment_id ON mining_extraction (equipment_id);
+CREATE INDEX idx_operator_id ON mining_extraction (operator_id);
+
+--Index Crushin Batches
+CREATE INDEX idx_crushing_batches_equipment_id ON crushing_batches (crusher_id);
+
+--Index hauling_trips
+CREATE INDEX idx_hauling_trips_equipment_id ON hauling_trips (truck_id);
+CREATE INDEX idx_hauling_trips_operator_id ON hauling_trips (driver_id);
+
+--Index sales_deliveries
+CREATE INDEX idx_sales_deliveries_equipment_id ON sales_deliveries (truck_id);
+CREATE INDEX idx_sales_deliveries_buyer_id ON sales_deliveries (buyer_id);
+
+--Index stockpile_movements
+CREATE INDEX idx_stockpile_movements_equipment_id ON stockpile_movements (source);
+
