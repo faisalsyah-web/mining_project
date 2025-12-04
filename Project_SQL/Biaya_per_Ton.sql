ac0b83efa30a90e0410b6ddffe457277ed20bbb2
faisalsyah-web <faisalsyahruddin57@gmail.com>
2025-12-04 23:14:49 +0900

    biaya_vs_grade

--- Project_SQL/Biaya_per_Ton.sql
+++ Project_SQL/Biaya_per_Ton.sql
@@ -1,53 +1,23 @@
-WITH biaya_volume as (
-SELECT
-    prod.pit_id,
-    prod.tanggal,
-    prod.equipment_id,
-    prod.jenis_produksi,
-    SUM(prod.volume_ton) as volume_ton,
-    SUM(prod.biaya_operasional_terkait) AS biaya_operasional
-FROM produksi prod
-GROUP BY
-    prod.pit_id,
-    prod.tanggal,
-    prod.equipment_id,
-    prod.jenis_produksi
-)
-   
-SELECT 
-    pit_id,
-    tanggal,
-    equipment_id,
-    jenis_produksi,
-    volume_ton,
-    biaya_operasional,
-    ROUND(biaya_operasional / volume_ton) as biaya_per_ton
-FROM biaya_volume
-GROUP BY
-    pit_id
-ORDER BY
-    pit_id
-
-
-
-
-
 WITH data_teragregasi_per_pit AS (
     SELECT
         prod.pit_id,
+        prod.tanggal,
         SUM(prod.volume_ton) AS total_volume_pit,
         SUM(prod.biaya_operasional_terkait) AS total_biaya_pit
     FROM produksi prod
     GROUP BY
-        prod.pit_id
+        prod.pit_id,
+        prod.tanggal
 )
 
 SELECT
     pit_id,
+    tanggal
     total_volume_pit,
     total_biaya_pit,
     ROUND(total_biaya_pit * 1.0 / total_volume_pit, 2) AS rata_rata_biaya_per_ton_pit
 FROM data_teragregasi_per_pit
-ORDER BY pit_id;
+ORDER BY tanggal
+
 
 
---
+++ Project_SQL/grade.sql
@@ -0,0 +1,30 @@
+WITH data_teragregasi_per_pit AS (
+    SELECT
+        prod.pit_id,
+        SUM(prod.volume_ton) AS total_volume_pit,
+        SUM(prod.biaya_operasional_terkait) AS total_biaya_pit
+    FROM produksi prod
+    GROUP BY prod.pit_id
+),
+
+biaya_per_ton AS (
+    SELECT
+        pit_id,
+        total_volume_pit,
+        total_biaya_pit,
+        ROUND(total_biaya_pit::numeric / NULLIF(total_volume_pit, 0), 2) AS rata_rata_biaya_per_ton_pit
+    FROM data_teragregasi_per_pit
+)
+
+SELECT
+    bpt.pit_id,
+    bpt.rata_rata_biaya_per_ton_pit,
+    ROUND(AVG(og.grade_pct), 2) AS rata_rata_grade
+FROM biaya_per_ton bpt
+JOIN ore_grade og
+    ON og.pit_id = bpt.pit_id
+GROUP BY
+    bpt.pit_id,
+    bpt.rata_rata_biaya_per_ton_pit
+ORDER BY
+    bpt.rata_rata_biaya_per_ton_pit DESC;
